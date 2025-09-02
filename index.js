7735c4cbe9c9d645b9ec544d73d0dbc1bc443eed
jpamoura <jean@solidapps.tech>
2025-09-02 15:33:58 -0300

    docker fix 3

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.use((req, res, next) => {
 // Middleware para log de requisições
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
+  console.log(`Headers:`, req.headers);
+  console.log(`Body:`, req.body);
   next();
 }); 
 
